196a4ce0ac31871a7a61620e9b68489cff91678d
Melanya25 <o.melanya@mail.ru>
2025-05-09 14:57:39 +0300

    Update dragon.h

--- dragon.h
+++ dragon.h
@@ -1,4 +1,4 @@
-#ifndef DRAGON_H
+#ifndef DRAGON_H // Предотвращение многократного включения заголовочного файла
 #define DRAGON_H
 
 #include <stdio.h>
@@ -6,17 +6,25 @@
 #include <time.h>
 #include <string.h>
 
+// Объявление структуры Dragon
 typedef struct {
-    char color[20];
-    int health;
-    int attack_min;
-    int attack_max;
-    int defense;
+    char color[20];  // Цвет дракона
+    int health;  // Здоровье дракона 
+    int attack_min; // Минимальный урон, который дракон может нанести
+    int attack_max; // Максимальный урон, который дракон может нанести
+    int defense; // Защита дракона
 } Dragon;
 
+// Прототип функции для инициализации параметров двух драконов (красного и синего)
 void init_dragons(Dragon* red, Dragon* blue);
+
+// Прототип функции для атаки одного дракона на другого
 void dragon_attack(Dragon* attacker, Dragon* defender);
+
+// Прототип функции для вывода на экран текущего состояния драконов (здоровье, цвет)
 void print_status(Dragon red, Dragon blue);
+
+// Прототип функции для проверки, есть ли победитель в битве
 int check_winner(Dragon red, Dragon blue);
 
 #endif
